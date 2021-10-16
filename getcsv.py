e7ebdb679fc60a472c43fdb5edb7013ede6a3fb1
Rosa Ruiz <ruizrivasrosacristina@gmail.com>
2021-10-16 23:26:47 +0200

    First draft of test analysis

--- getcsv.py
+++ getcsv.py
@@ -1,18 +1,20 @@
-import os
 import csv
 from analyzer import calcular_puntuacion
 import glob
-import xml
+
+
+PATH = "/media/rcruiz/TOSHIBA EXT/snap_projects/snap-projects/**/**/project.xml"
+PATH2 = "/home/rcruiz/Downloads/prueba_snap/**/**/project.xml"
+
 
 def save_to_csv():
-
-    with open('results_snap.csv', 'a', newline='') as csvfile:
-        fieldnames = ['Project', 'Path' ,'Level', 'Score', 'Average', 'Conditional',
+    with open('results_snap_test.csv', 'a', newline='') as csvfile:
+        fieldnames = ['Project', 'Path', 'Level', 'Score', 'Average', 'Conditional',
                       'Synchronization', 'Flow Control', 'Abstraction', 'Parallelism',
                       'Diversity', 'User Interactivity', 'Data']
         writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(fieldnames)
-        for filename in glob.glob("/media/rcruiz/TOSHIBA EXT/snap_projects/snap-projects/**/**/project.xml", recursive=False):
+        for filename in glob.glob(PATH2, recursive=False):
             writer.writerow(calcular_puntuacion(filename))
 
 
